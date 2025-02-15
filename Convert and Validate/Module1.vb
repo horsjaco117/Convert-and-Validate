78df457c5455203c782ac087f41363768e9924e6
horsjaco117 <jacob_horsley@live.com>
2025-02-15 14:41:44 -0700

    Add project files.

---
+++ Convert and Validate/Module1.vb
@@ -0,0 +1,52 @@
+﻿Module Module1
+
+    Sub Main()
+
+        'for testing the function add this to your Sub Main()
+
+        Dim aValidNumber As Integer
+        Dim userResponse As String
+
+        Do
+
+            Console.WriteLine($"Enter a number:")
+
+            userResponse = Console.ReadLine()
+
+            If ConversionValid(userResponse, aValidNumber) = True Then
+
+                Console.WriteLine($"{userResponse} converted successfully to {aValidNumber}!")
+
+            Else
+
+                Console.WriteLine($"Oops, {userResponse} does not seem to be a number")
+
+            End If
+
+        Loop
+
+    End Sub
+
+    'make this work
+
+
+    Function ConversionValid(convertThisString As String, ByRef toThisInteger As Integer) As Boolean
+
+        Dim status As Boolean
+
+        Try
+            toThisInteger = convertThisString.(convertThisString)
+            Return True
+
+        Catch ex As Exception
+            toThisInteger = 0 'Reset the output variable to failure
+            Return False
+
+        End Try
+
+        Return status
+
+    End Function
+
+
+End Module
